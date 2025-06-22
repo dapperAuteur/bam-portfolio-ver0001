72d02e5438b9693784dc387bd27ccbab0af6560a
awe <aweful@awews.com>
2025-06-21 21:33:33 -0400

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           </main>
           <Footer />
         </div>
+        <Analytics />
       </body>
     </html>
   );
